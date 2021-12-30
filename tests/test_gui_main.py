febff4791ff929e4cd87ad00093698b09f441e8d
Paul Müller <dev@craban.de>
2021-12-30 22:25:23 +0100

    tests: increase coverage

--- tests/test_gui_main.py
+++ tests/test_gui_main.py
@@ -1,13 +1,33 @@
+import io
 import pathlib
+from unittest import mock
 
-from PyQt5 import QtWidgets
+import pytest
+from PyQt5 import QtCore, QtWidgets
 
+import dctag
 from dctag.gui.main import DCTag
 from dctag import session
 from helper import get_clean_data_path
 
 
 data_dir = pathlib.Path(__file__).parent / "data"
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    # Code that will run before your test, for example:
+    pass
+    # A test function will be run at this point
+    yield
+    # Code that will run after your test, for example:
+    # restore dctag-tester for other tests
+    QtCore.QCoreApplication.setOrganizationName("MPL")
+    QtCore.QCoreApplication.setOrganizationDomain("mpl.mpg.de")
+    QtCore.QCoreApplication.setApplicationName("dctag")
+    QtCore.QSettings.setDefaultFormat(QtCore.QSettings.IniFormat)
+    settings = QtCore.QSettings()
+    settings.setValue("user/name", "dctag-tester")
 
 
 def test_basic(qtbot):
@@ -38,3 +58,52 @@ def test_clear_session(qtbot):
     assert not mw.tab_binary.session
     assert not mw.tab_binary.widget_vis.session
     mw.tabWidget.setCurrentIndex(2)
+
+
+def test_init_get_username(qtbot):
+    # first reset the username
+    # (undo what was done in conftest.py)
+    QtCore.QCoreApplication.setOrganizationName("MPL")
+    QtCore.QCoreApplication.setOrganizationDomain("mpl.mpg.de")
+    QtCore.QCoreApplication.setApplicationName("dctag")
+    QtCore.QSettings.setDefaultFormat(QtCore.QSettings.IniFormat)
+    settings = QtCore.QSettings()
+    settings.remove("user/name")
+
+    with mock.patch.object(QtWidgets.QInputDialog, "getText",
+                           return_value=("peter", True)):
+        DCTag()
+
+    assert settings.value("user/name") == "peter"
+
+
+def test_init_get_username_abort(qtbot):
+    # first reset the username
+    # (undo what was done in conftest.py)
+    QtCore.QCoreApplication.setOrganizationName("MPL")
+    QtCore.QCoreApplication.setOrganizationDomain("mpl.mpg.de")
+    QtCore.QCoreApplication.setApplicationName("dctag")
+    QtCore.QSettings.setDefaultFormat(QtCore.QSettings.IniFormat)
+    settings = QtCore.QSettings()
+    settings.remove("user/name")
+
+    with mock.patch.object(QtWidgets.QInputDialog, "getText",
+                           return_value=("hans", False)):
+        with pytest.raises(SystemExit):
+            DCTag()
+
+    assert settings.value("user/name") is None
+
+
+def test_init_print_version(qtbot):
+    mock_stdout = io.StringIO()
+    mock_exit = mock.Mock()
+
+    with mock.patch("sys.argv", ["--version"]):
+        with mock.patch("sys.exit", mock_exit):
+            with mock.patch('sys.stdout', mock_stdout):
+                mw = DCTag("--version")
+                mw.close()
+
+    assert mock_exit.call_args.args[0] == 0
+    assert mock_stdout.getvalue().strip() == dctag.__version__
