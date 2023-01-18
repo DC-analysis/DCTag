de4cbdbf7eb7396e759ca38c5d594a5181c86310
nsbaa <nadia.sbaa@mpl.mpg.de>
2023-01-18 16:02:19 +0100

    Following - Delete unnecessary functions

--- dctag/gui/simple_plot_widget.py
+++ dctag/gui/simple_plot_widget.py
@@ -1,20 +1,17 @@
-from PyQt5 import QtCore, QtWidgets
 import pyqtgraph as pg
 
 
 class SimplePlotItem(pg.PlotItem):
-    """Custom class for data visualization
+    """Custom class for data visualization in dctag.
 
     Modifications include:
-    - right click menu only with "Export..."
     - top and right axes
     """
 
     def __init__(self, parent=None, *args, **kwargs):
         if "viewBox" not in kwargs:
-            kwargs["viewBox"] = SimpleViewBox()
+            kwargs["viewBox"] = pg.ViewBox()
         super(SimplePlotItem, self).__init__(parent, *args, **kwargs)
-        self.vb.export.connect(self.on_export)
         # show top and right axes
         for kax in ["top", "right"]:
             self.showAxis(kax)
@@ -29,20 +26,9 @@ class SimplePlotItem(pg.PlotItem):
                         )
         self.hideButtons()
 
-    def on_export(self, suffix):
-        """Export subplots as original figures (with axes labels, etc)"""
-        file, _ = QtWidgets.QFileDialog.getSaveFileName(
-            None,
-            'Save {} file'.format(suffix.upper()),
-            '',
-            '{} file (*.{})'.format(suffix.upper(), suffix))
-        if not file.endswith("." + suffix):
-            file += "." + suffix
-        self.perform_export(file)
-
 
 class SimplePlotWidget(pg.PlotWidget):
-    """Custom class for data visualization in Shape-Out
+    """Custom class for data visualization in dctag.
 
     Modifications include:
     - white background
@@ -54,16 +40,3 @@ class SimplePlotWidget(pg.PlotWidget):
         super(SimplePlotWidget, self).__init__(parent,
                                                background=background,
                                                plotItem=plot_item)
-
-
-class SimpleViewBox(pg.ViewBox):
-    export = QtCore.pyqtSignal(str)
-
-    def __init__(self, *args, **kwargs):
-        super(SimpleViewBox, self).__init__(*args, **kwargs)
-        #: allowed right-click menu options with new name
-        self.right_click_actions = {}
-        settings = QtCore.QSettings()
-        if int(settings.value("advanced/developer mode", 0)):
-            # Enable advanced export in developer mode
-            self.right_click_actions["Export..."] = "Advanced Export"
